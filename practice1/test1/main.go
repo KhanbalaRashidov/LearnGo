75c187e13fd0c94023d359d5e71e3cc9d5f8272d
KhanbalaRashidov <reshidovxanbala@gmail.com>
2022-11-15 23:58:21 +0300

    practice

---
+++ practice1/test1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "Khanbala Rashidov"
+	z := true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
---
+++ practice1/test2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x) //zeero value =0
+	fmt.Println(y) // zero value empty
+	fmt.Println(z) //zero value = false
+}
