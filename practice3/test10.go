33eafb49b8f04f946d955b3d2a2d10377e4dfd60
KhanbalaRashidov <reshidovxanbala@gmail.com>
2022-11-25 16:05:19 +0300

    add

---
+++ practice3/test10.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	fmt.Println(false && false)
+	fmt.Println(false && true)
+	fmt.Println(false || false)
+	fmt.Println(false || true)
+	fmt.Println(!false)
+}
